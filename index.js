1cdaa02f18b0c9e36aa74cc43f4e4a94fe95778e
James_Devfong <james@mail.com>
2021-07-05 10:19:51 +0700

    edit db connect url

--- index.js
+++ index.js
@@ -1,20 +1,65 @@
 const express = require('express')
 require('dotenv').config()
 const mongoose = require('mongoose')
+const mongoClient = require('mongodb').MongoClient
 const cors = require("cors")
 const path = require("path")
 const app = express()
 
-mongoose.connect(
-  process.env.DB_URL,
-  {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  },
-  () => console.log('connected database')
-)
+// mongoose.connect(
+//   process.env.DB_URL,
+//   {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false
+//   },
+//   (err,db) => {
+//     if(err){
+//       console.log(err)
+//       db.close()
+//     }
+//     console.log("connected database")
+//   }
+// )
+
+// mongoose.createConnection(
+//   //"mongodb://localhost:27017/dbName",
+//   "mongodb://node15454-shift.th.app.ruk-com.cloud:27017/shift_cafe",
+//   {
+//     "auth": {
+//       "authSource": "admin"
+//     },
+//     "user": "admin",
+//     "pass": "ZIPhir37741"
+    
+//   }
+// );
+
+const Connect = async () => {
+
+  let url = "mongodb://node15454-shift.th.app.ruk-com.cloud:27017/shift_cafe";
+
+  try {
+
+      let client = await mongoose.connect( url, {
+          poolSize: 10,
+          authSource: "admin",
+          user: "admin",
+          pass: "ZIPhir37741", 
+          useCreateIndex: true,
+          useNewUrlParser: true,
+          useUnifiedTopology: true
+      } );
+
+      console.log( "Database is connected!" );
+  } catch ( error ) {
+      console.log( error.stack );
+      process.exit( 1 );
+  }
+
+}
+Connect();
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
