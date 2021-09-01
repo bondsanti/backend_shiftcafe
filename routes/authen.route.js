2e48b56d209bb8c0f69e615d9b44513c42853141
JA EM <james@devfong.co.th>
2021-09-01 15:01:07 +0700

    improve14

--- routes/authen.route.js
+++ routes/authen.route.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.post("/authen/login",login)
 //router.post("/authen/login2",login2)
-router.post("/authen/logout",logout)
+router.post("/authen/logout",requireLogin,logout)
 router.get("/authen/user",requireLogin,getUser)
 router.post("/authen/request-otp",requireLogin,requireAdmin,requestOTP)
 router.post("/authen/verify-otp",requireLogin,requireAdmin,verifyOTP)
